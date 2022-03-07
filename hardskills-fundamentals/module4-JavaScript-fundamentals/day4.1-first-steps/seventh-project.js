3c1d7c6c90c1c0631056653bc243ca599bcbd81b
EderBorella <ederlopesborella@gmail.com>
2022-03-07 21:29:11 +0000

    Seventh project.

---
+++ hardskills-fundamentals/module4-JavaScript-fundamentals/day4.1-first-steps/seventh-project.js
@@ -0,0 +1,17 @@
+let statusPessoa = "listada"
+
+switch(statusPessoa){
+    case "aprovada":
+        console.log(statusPessoa)
+        break;
+    
+    case "reprovada":
+        console.log(statusPessoa)
+        break;
+    
+    case "listada":
+        console.log(statusPessoa)
+        break;
+    default:
+        console.log("não listado.")
+}
