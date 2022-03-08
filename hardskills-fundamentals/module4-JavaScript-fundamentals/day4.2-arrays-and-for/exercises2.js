ca44af261e09f1315ae1d6a05e897a0efb6e6bc4
EderBorella <ederlopesborella@gmail.com>
2022-03-08 21:27:32 +0000

    Exercicios 3 a 7.

--- hardskills-fundamentals/module4-JavaScript-fundamentals/day4.2-arrays-and-for/exercises2.js
+++ hardskills-fundamentals/module4-JavaScript-fundamentals/day4.2-arrays-and-for/exercises2.js
@@ -1,5 +1,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let numberSum = 0;
+let maxNumb = 0;
+let odds = [];
 
 // Exercicio 1:
 
@@ -20,3 +22,54 @@ let numberSum = 0;
 // console.log(numberSum)
 
 // Exercicio 3:
+
+// for(let number of numbers){
+//     numberSum += number;
+// }
+// console.log(numberSum / numbers.length)
+
+// for(let number of numbers){
+//     numberSum += number;
+// }
+// console.log(numberSum / numbers.length)
+
+// Exercicio 4:
+
+// for(let number of numbers){
+//     numberSum += number;
+// }
+// if(numberSum / numbers.length > 20){
+//     console.log('Valor maior que 20.')
+// }else{
+//     console.log('Valor menor que ou igual 20.')
+// }
+
+// Exercicio 5:
+
+// for(let i = 0; i < numbers.length; i += 1){
+//     if(maxNumb < numbers[i]){
+//         maxNumb = numbers[i]
+//     }
+// }
+// console.log(maxNumb)
+
+// Exercicio 6:
+
+// for(let i = 0; i < numbers.length; i += 1){
+//     if(numbers[i] % 2 !== 0){
+//         odds.push(numbers[i])
+//     }
+// }
+// console.log(odds)
+
+// Exercicio 7:
+
+// for(let i = 0; i < numbers.length; i += 1){
+//     if(numbers[0] > numbers[i]){
+//         maxNumb = numbers[i]
+//     }
+// }
+// console.log(maxNumb)
+
+// Exercicio 8:
+
