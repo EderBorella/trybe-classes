f615aa6a785f5a9657c27736db14be0163a1d582
EderBorella <ederlopesborella@gmail.com>
2022-03-14 21:38:04 +0000

    Exercicio 5.1 - Requisito 1.

---
+++ hardskills-fundamentals/module5-DOM-events-and-web-storage/practice.js
@@ -0,0 +1,7 @@
+let ptext = document.getElementsByTagName('p')[0];
+
+function textChange(posi, text){
+    posi.innerText = text;
+};
+
+textChange(ptext, 'Me vejo trabalhando com programação, em uma empresa grande.')
