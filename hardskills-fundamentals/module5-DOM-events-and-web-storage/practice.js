881bbd987549c8e9beffca5e20171fc5e027c0bf
EderBorella <ederlopesborella@gmail.com>
2022-03-14 21:50:33 +0000

    Requisito 4.

--- hardskills-fundamentals/module5-DOM-events-and-web-storage/practice.js
+++ hardskills-fundamentals/module5-DOM-events-and-web-storage/practice.js
@@ -1,6 +1,8 @@
 let ptext = document.getElementsByTagName('p')[0];
 let box1 = document.getElementsByClassName('main-content')[0]
 let box2 = document.getElementsByClassName('center-content')[0]
+let wrongTitle = document.getElementsByClassName('title')[0]
+
 
 function textChange(posi, text){
     posi.innerText = text;
@@ -11,7 +13,11 @@ function changeCollor(posi2, color){
 function changeCollor2(posi3, color2){
     posi3.style.background = color2
 }
+function correctText(posi4, text2){
+    posi4.innerText = text2;
+}
 
 textChange(ptext, 'Me vejo trabalhando com programação, em uma empresa grande.')
 changeCollor(box1, "rgb(76,164,109)")
 changeCollor2(box2, 'white')
+correctText(wrongTitle, 'Exercício 5.1 - JavaScript')
