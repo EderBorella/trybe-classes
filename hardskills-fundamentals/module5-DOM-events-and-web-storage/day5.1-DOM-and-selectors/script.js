e8ba0422cfc3d141add082f257230eb7ebad0b4a
EderBorella <ederlopesborella@gmail.com>
2022-03-15 17:10:01 +0000

    Ajusted directory

---
+++ hardskills-fundamentals/module5-DOM-events-and-web-storage/day5.1-DOM-and-selectors/script.js
@@ -0,0 +1,9 @@
+document.querySelector("#page-title").innerText = 'The Goodfather';
+document.querySelector("#subtitle").innerText = 'Mais que um filme, um movie.';
+
+const firstPClass = document.querySelectorAll(".paragraph")[0]
+firstPClass.style.color = 'red'
+
+const firstH4 = document.querySelector("h4")
+
+firstH4.style.background = 'pink'
