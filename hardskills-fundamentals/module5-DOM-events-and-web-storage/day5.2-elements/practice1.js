608e0812b540a0f21b8133c93d98591db9ea52b4
EderBorella <ederlopesborella@gmail.com>
2022-03-15 18:02:30 +0000

    Practice part 1.

---
+++ hardskills-fundamentals/module5-DOM-events-and-web-storage/day5.2-elements/practice1.js
@@ -0,0 +1,14 @@
+let res1 = document.getElementById('primeiroFilho').nextElementSibling.firstElementChild;
+
+res1.innerText = 'Texto alterado, pae.';
+
+let res2 = document.getElementById('pai').firstElementChild;
+
+let res3 = document.getElementById('elementoOndeVoceEsta').previousElementSibling;
+
+let res4 = document.getElementById('elementoOndeVoceEsta').nextSibling;
+
+let res5 = document.getElementById('elementoOndeVoceEsta').nextElementSibling;
+
+let res6 = document.getElementById('pai').children[2];
+
