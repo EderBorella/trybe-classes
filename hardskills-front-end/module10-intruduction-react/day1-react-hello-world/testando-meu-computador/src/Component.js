6bf41761a698a1f126284747b776dac554968576
EderBorella <ederlopesborella@gmail.com>
2022-05-06 18:42:30 +0100

    First exercise.

---
+++ hardskills-front-end/module10-intruduction-react/day1-react-hello-world/testando-meu-computador/src/Component.js
@@ -0,0 +1,10 @@
+function Component() {
+  return (
+    <div>
+      <h1>Eder Lopes Borella</h1>
+      <p>Tenho 27 anos, moro em Londres, amo programação, games e mundos de fantasia.</p>
+    </div>);
+}
+
+export default Component;
+
