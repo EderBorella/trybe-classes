8c83d280a5c96f53774264644b603524b4f4a016
EderBorella <ederlopesborella@gmail.com>
2022-05-11 18:09:02 +0100

    React button with state.

--- hardskills-front-end/module11-state-events-forms/day1-events-state/fancy-buttons/src/App.js
+++ hardskills-front-end/module11-state-events-forms/day1-events-state/fancy-buttons/src/App.js
@@ -2,12 +2,12 @@ import './App.css';
 import React from 'react';
 
 class App extends React.Component {
+  state = {count: 0};
+
+  increment = () => this.setState({ count: this.state.count + 1})
+
   render() {
-    return (
-      <div className="App">
-        'Learn React'
-      </div>
-    )
+    return <button onClick={this.increment}>Meus cliques: {this.state.count}</button>
   }
 }
 
